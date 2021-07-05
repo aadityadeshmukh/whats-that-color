64bb9f9be65c420c213191f52761a3d9a601c8e5
aadityadeshmukh <aadityadeshmukh@gmail.com>
2021-07-05 12:05:16 +0530

    color name from color input

--- script.js
+++ script.js
@@ -9,4 +9,15 @@ function changeColor() {
   let col = colorSelection.value;
   console.log(col);
   decoyColorSelector.style.color = col;
+
+  //get the name of the color
+  let n_match = ntc.name(col);
+  n_rgb = n_match[0];
+  n_name = n_match[1];
+  n_exactMatch = n_match[2];
+  console.log(n_name);
+
+  //set the color name to paragraph elem
+  let colorNameElem = document.getElementById('colorName');
+  colorNameElem.textContent = n_name;
 }
